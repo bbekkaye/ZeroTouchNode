7ffc9785695f2a8cccca9a8c74cf0981d37fffb1
Bachir Bekkaye. <bachirbk@gmail.com>
2024-06-24 05:01:23 -0700

    Add input to zerotouch node

--- src/ZeroTouchNode/ZeroTouchNode/MyNodes/MyFirstNode.cs
+++ src/ZeroTouchNode/ZeroTouchNode/MyNodes/MyFirstNode.cs
@@ -6,14 +6,22 @@ using System.Threading.Tasks;
 
 namespace ZeroTouchNode
 {
+    /// <summary>
+    /// Wrapper class for my first node
+    /// </summary>
     public class MyFirstNode
     {
         // This hides the overall class as a node
         private MyFirstNode() { }
-        public static string HelloWorld()
+
+        /// <summary>
+        /// This is my first node that outputs a string that says Hello world. Awesome.
+        /// </summary>
+        /// <returns name="helloWorldString">Our hello world node.</returns>
+        public static string HelloWorld(string username)
         {
             //returns one output of hello world
-            return "Hello world";
+            return "Hello " + username;
         }
     }
 }
